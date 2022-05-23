466b4c0899ecca1cc9b391358474794545cf5769
seanhaythorne <seanhaythorne@hotmail.com>
2022-05-23 18:01:54 +1000

    tests (cont.)

--- tests/testthat/test_incursions.R
+++ tests/testthat/test_incursions.R
@@ -1,5 +1,42 @@
 context("Incursions")
 
-test_that("test 1", {
-  expect_true(TRUE)
+test_that("initializes with raster layer", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  region <- Region(template*0)
+  incursion_rast <- template*10
+  expect_silent(incursions <- Incursions(incursion_rast, region))
+  expect_is(incursions, "Incursions")
+  expect_equal(incursions$get_type(), "weight")
+  expect_error(incursions <- Incursions(incursion_rast, 1:10),
+               "Region model must be a 'Region' or inherited class object.")
+  expect_error(incursions <- Incursions(1:10, region),
+               paste("Vector x length must be equal to the number of region",
+                     "locations."))
 })
+
+test_that("weighted incursion location", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  region <- Region(template*0)
+  incursion_rast <- template*10
+  incursions <- Incursions(incursion_rast, region, type = "weight")
+  expect_silent(generated_incursion <- incursions$generate())
+  expect_is(generated_incursion, "logical")
+  expect_equal(length(which(generated_incursion)), 1)
+})
+
+test_that("generates probability incursions", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  region <- Region(template*0)
+  incursion_rast <- template
+  incursions <- Incursions(incursion_rast, region, type = "prob")
+  mean_incursions <- (mean(template[region$get_indices()][,1])*
+                        region$get_locations())
+  expect_silent(generated_incursions <- incursions$generate())
+  expect_is(generated_incursions, "logical")
+  expect_true(abs(length(which(generated_incursions)) -
+                    mean_incursions)/mean_incursions < 0.1)
+})
+
--- tests/testthat/test_initializer.R
+++ tests/testthat/test_initializer.R
@@ -1,5 +1,43 @@
 context("Initializer")
 
-test_that("test 1", {
-  expect_true(TRUE)
+test_that("initializes with raster layer", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  region <- Region(template)
+  initial_rast <- round(template*10)
+  expect_error(initializer <- Initializer(1:10, region),
+               paste("Vector (or array) x length (or number of rows) must be",
+                     "equal to the number of region locations."), fixed = TRUE)
+  expect_error(initializer <- Initializer(initial_rast, 1:10),
+               "Region model must be a 'Region' or inherited class object.")
+  expect_error(initializer <- Initializer(initial_rast, region,
+                                          population_model = 1:10),
+               "Population model must be a 'Population' or inherited class object.")
+  pop_model <- Population(region)
+  expect_silent(initializer <- Initializer(initial_rast, region,
+                                           population_model = pop_model))
+  expect_is(initializer, "Initializer")
+  expect_silent(n <- initializer$initialize())
+  expect_equal(n, initial_rast[region$get_indices()][,1])
+})
+
+test_that("initializes with incursion object", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  region <- Region(template)
+  incursion_rast <- template/10
+  incursions <- Incursions(incursion_rast, region, type = "prob",
+                           continued = TRUE)
+  pop_model <- Population(region)
+  expect_silent(initializer <- Initializer(incursions, region,
+                                           population_model = pop_model))
+  expect_silent(n <- initializer$initialize())
+  idx <- which(n)
+  expect_true(length(idx) > 1)
+  expect_is(initializer$continued_incursions, "function")
+  expect_silent(continued_incursions <- initializer$continued_incursions())
+  expect_is(continued_incursions, "function")
+  expect_silent(n <- continued_incursions(tm = 2, n))
+  expect_true(length(which(n)) > length(idx))
+  expect_true(all(idx %in% which(n)))
 })
