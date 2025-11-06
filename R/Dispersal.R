6d268e60b81bc25de4c711ad70f1d2fe916dc1d3
seanhaythorne <seanhaythorne@hotmail.com>
2025-11-06 13:54:50 +1100

    Avoid applying dynamic multiplier repeatedly when multiple attractors

--- R/Dispersal.R
+++ R/Dispersal.R
@@ -362,9 +362,16 @@ Dispersal.Region <- function(region, population_model,
           }
         }
       }
+      num_dynamic <- sum(sapply(attractors, function(a) {
+        if (is.function(a$get_is_dynamic)) {
+          a$get_is_dynamic()
+        } else {
+          FALSE
+        }
+      }))
       for (attractor in attractors) {
         if (inherits(attractor, "Attractor") && attractor$get_is_dynamic()) {
-          attractor$set_multiplier(dynamic_mult)
+          attractor$set_multiplier(dynamic_mult^(1/num_dynamic))
         }
       }
     }
